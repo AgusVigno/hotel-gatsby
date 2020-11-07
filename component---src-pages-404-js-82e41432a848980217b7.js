bb8a0578962ee885bb9a1a9cda0f683cf98a69fe
Agustin Vignolo <agus88lp@gmail.com>
2020-11-07 12:30:23 -0300

    Updates

---
+++ component---src-pages-404-js-82e41432a848980217b7.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{w2l6:function(t,e,n){"use strict";n.r(e);n("q1tI");var s=n("Bl7J"),u=n("qKvR");e.default=function(){return Object(u.c)(s.a,null,Object(u.c)("h1",null,"404: No existe la página"),Object(u.c)("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-82e41432a848980217b7.js.map
